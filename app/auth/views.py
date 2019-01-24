3f285110cdba89eaf1b4ccf231fd9349071c2722
Anguandia <mikeanguandia@gmail.com>
2019-01-24 04:16:32 +0300

    add login view

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,5 @@
 from flask import jsonify, request
 from . import bp
-from app import db, validation
 from app.validation import Validation
 # from app.wrappers import admin_required, auth_required
 
@@ -8,5 +7,16 @@ from app.validation import Validation
 @bp.route('/signup', methods=['POST'])
 def signup():
     data = request.json
-    ans = Validation().validateSignup(data)
-    return jsonify({'Status': ans[0], ans[1]: ans[2]}), ans[0]
+    resp = Validation().validate_signup(data)
+    return display(resp)
+
+
+@bp.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    resp = Validation().validate_login(data)
+    return display(resp)
+
+
+def display(resp):
+    return jsonify({'Status': resp[0], resp[1]: resp[2]}), resp[0]
